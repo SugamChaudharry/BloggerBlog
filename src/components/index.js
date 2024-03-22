7dc9b3d71593156fadfb8ca3ae0471cfa15ae050
SugamChaudharry <nextgenachieverss@gmail.com>
2024-03-22 23:19:39 +0530

    Add new components AuthLayout, RET, and PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer.jsx";
 import Container from "./Container/container.jsx";
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RET.jsx"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
